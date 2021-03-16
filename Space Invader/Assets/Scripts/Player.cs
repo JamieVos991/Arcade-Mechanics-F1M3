4f54326fb914a0cb3e2b33926d031bc1d96b1a4e
Jamie <31694@ma-web.nl>
2021-03-16 11:37:27 +0100

    Unity Dingen

--- Space Invader/Assets/Scripts/Player.cs
+++ Space Invader/Assets/Scripts/Player.cs
@@ -4,26 +4,42 @@ using UnityEngine;
 
 public class Player : MonoBehaviour
 {
-
+    public GameObject Player;
+    public GameObject projectile;
+    public GameObject projectileClone;
     public int speed = 5;
 
-    // Start is called before the first frame update
     void Start()
     {
-        
+
+    }
+
+    // Start is called before the first frame update
+    void Update()
+    {
+        movement();
+        fireProjectile();
     }
 
     // Update is called once per frame
-    void Update()
+    void movement()
     {
         if (Input.GetKey(KeyCode.A))
         {
-            transform.Translate(new Vector3(speed * Time.deltaTime, 0f, 0f));
+            transform.Translate(new Vector3(-speed * Time.deltaTime, 0f, 0f));
         }
         else if (Input.GetKey(KeyCode.D))
         {
-            transform.Translate(new Vector3(-speed * Time.deltaTime, 0f, 0f));
+            transform.Translate(new Vector3(speed * Time.deltaTime, 0f, 0f));
         }
 
+    }
+    
+    void fireProjectile()
+    {
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            projectileClone = Instantiate(fireProjectile, new Vector3(0, 0, 0), transform.rotation) as GameObject;
+        }
     }
 }
