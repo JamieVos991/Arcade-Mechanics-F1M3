10ca0682103dece48269d5ee76a1cd7f4cb7ac79
Jamie <31694@ma-web.nl>
2021-03-22 15:32:13 +0100

    Art dingetjes

--- Space Invader/Assets/Scripts/PlayerCollision.cs
+++ Space Invader/Assets/Scripts/PlayerCollision.cs
@@ -7,8 +7,9 @@ public class PlayerCollision : MonoBehaviour
 
     private void OnTriggerEnter2D(Collider2D other)
     {
-        Debug.Log("HIT");
-        Destroy(other.gameObject);
-        this.gameObject.SetActive(false);
+            Debug.Log("HIT");
+            Destroy(other.gameObject);
+            this.gameObject.SetActive(false);
+        
     }
 }
